cc2726901d5c7e04a4e642e5b887e41b5bae5eea
Konrad Bajtyngier <konrad.bajtyngier@gmail.com>
2017-04-21 14:58:35 +0200

    First substantial commit. Contains the first implementation of the SKButtonNode and a set of examples.

--- SKButtonNode/SKButtonNode/GameScene.swift
+++ SKButtonNode/SKButtonNode/GameScene.swift
@@ -10,80 +10,53 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+	
+	let texture1 = SKTexture(imageNamed: "button1")
+	let texture2 = SKTexture(imageNamed: "button2")
+	
+	override func didMove(to view: SKView) {
+		
+		
+		
+		//Exmaple 1 - simple example with texture and text
+		
+		let button1 = SKButtonNode(texture: texture1) {
+			print("Button 1 pressed!")
+		}
+		button1.setTitle("Example 1")
+		button1.position = CGPoint(x: view.frame.midX, y: view.frame.height-100)
+		addChild(button1)
+		
+		
+		
+		//Example 2 - example with different highlighetd state
+		
+		let button2 = SKButtonNode(texture: texture1, title: "Example 2") {
+			print("Button 2 pressed!")
+		}
+		button2.titleLabel?.fontSize = 15
+		button2.titleLabel?.fontName = "Avenir-Black"
+		button2.setTexture(texture2, for: .highlighted)
+		button2.setTitleColor(SKColor.red, for: .highlighted)
+		button2.position = CGPoint(x: view.frame.midX, y: view.frame.height-200)
+		addChild(button2)
+		
+		
+		
+		//Example 3 - button based on a shape, not image
+		
+		let shape = SKShapeNode(circleOfRadius: 40)
+		shape.fillColor = SKColor.brown
+		shape.strokeColor = SKColor.white
+		if let button3 = SKButtonNode(view: view, shape: shape, title: "Ok") {
+			button3.action = {
+				print("Button 3 pressed!")
+			}
+			button3.setTitleColor(SKColor.black, for: .highlighted)
+			button3.position = CGPoint(x: view.frame.midX, y: view.frame.height-350)
+			addChild(button3)
+		}
+		
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+	
 }
---
+++ SKButtonNode/SKButtonNode/SKButtonNode.swift
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2017 Konrad Bajtyngier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import SpriteKit
+
+///Corresponds to button's current control state. Very much like UIControlState in case of UIButton.
+public enum SKButtonNodeState {
+	case normal
+	case highlighted
+}
+
+///Simple implementation of button for SpriteKit. Derives much of its concepts from UIKit's UIButton.
+public class SKButtonNode: SKNode {
+	
+	//MARK: - Public
+	
+	public var action:(()->())?
+	public var enabled = true
+	
+	//MARK: - Private(set)
+	
+	private(set) var titleLabel:SKLabelNode?
+	private(set) var state:SKButtonNodeState = .normal {
+		didSet {
+			updateState()
+		}
+	}
+	
+	//MARK: - Computed
+	
+	public var title:String? {
+		return titleLabel?.text
+	}
+	public var currentTexture:SKTexture? {
+		return sprite.texture
+	}
+	public var size:CGSize {
+		return sprite.size
+	}
+	
+	//MARK: - Private
+	
+	private var sprite:SKSpriteNode
+	private var activated = false
+	private var normalState:State
+	private var highlightedState:State?
+	
+	//MARK: - Initialization
+	
+	/**
+	Creates a button with a texture generated from the specified image.
+	- parameter imageNamed: name of the image from assets catalog
+	- parameter title: button's label text
+	- parameter action: closure to be called as the button's action
+	*/
+	convenience init(imageNamed:String, title:String? = nil, action:(()->())? = nil) {
+		let texture = SKTexture(imageNamed: imageNamed)
+		self.init(texture: texture, title: title, action: action)
+	}
+	
+	/**
+	Failing initializer that creates a button with a texture generated from the passed SKShapeNode.
+	- parameter view: the SKView instance to be used for generating a texture from shape node
+	- parameter shape: the shape that will be used to generate a texture from
+	- parameter title: button's label text
+	- parameter action: closure to be called as the button's action
+	*/
+	convenience init?(view:SKView, shape:SKShapeNode, title:String? = nil, action:(()->())? = nil) {
+		guard let texture = view.texture(from: shape) else { return nil }
+		self.init(texture: texture, title: title, action: action)
+	}
+	
+	/**
+	Creates a button with a passed texture.
+	- parameter texture: texture object, which will make up the button
+	- parameter title: button's label text
+	- parameter action: closure to be called as the button's action
+	*/
+	init(texture:SKTexture, title:String? = nil, action:(()->())? = nil) {
+		self.sprite = SKSpriteNode(texture: texture)
+		self.action = action
+		self.normalState = State(texture: texture)
+		super.init()
+		isUserInteractionEnabled = true
+		addChild(sprite)
+		setTitle(title)
+	}
+	
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func updateState() {
+		//Determine current state of the button
+		var currentState:State
+		switch self.state {
+		case .highlighted:
+			currentState = highlightedState ?? normalState
+		default:
+			currentState = normalState
+		}
+		//Update the button to match the current state
+		sprite.texture = currentState.texture
+		titleLabel?.fontColor = currentState.fontColor
+	}
+	
+	//MARK: - Texture
+	
+	///Takes the image name and created a texture to further update the specified state with
+	public func setImage(named:String, for state:SKButtonNodeState) {
+		let texture = SKTexture(imageNamed: named)
+		setTexture(texture, for: state)
+	}
+	
+	///Update the specified state with the specified texture
+	public func setTexture(_ texture:SKTexture, for state:SKButtonNodeState) {
+		switch state {
+		case .normal:
+			normalState.texture = texture
+		case .highlighted:
+			if let _ = highlightedState {
+				highlightedState!.texture = texture
+			} else {
+				highlightedState = State(texture: texture, fontColor: normalState.fontColor)
+			}
+		}
+		updateState()
+	}
+	
+	//MARK: - Title Text
+	
+	public func setTitle(_ title:String?) {
+		//If the title is empty, the label child is removed
+		guard let title = title, title != "" else {
+			titleLabel?.removeFromParent()
+			titleLabel = nil
+			return
+		}
+		//If the label child was previously created, update the text
+		if let label = titleLabel {
+			label.text = title
+		}
+		//If the title was previously empty, create a new label
+		else {
+			titleLabel = SKLabelNode(text: title)
+			titleLabel!.verticalAlignmentMode = .center
+			titleLabel!.zPosition = 1
+			addChild(titleLabel!)
+		}
+	}
+	
+	//MARK: - Title Color
+	
+	public func setTitleColor(_ color:SKColor, for state:SKButtonNodeState) {
+		switch state {
+		case .normal:
+			normalState.fontColor = color
+		case .highlighted:
+			if let _ = highlightedState {
+				highlightedState!.fontColor = color
+			} else {
+				highlightedState = State(texture: normalState.texture, fontColor: color)
+			}
+		}
+		updateState()
+	}
+	
+	//MARK: - User Interaction Handling
+	
+	override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let _ = touches.first, enabled else { return }
+		state = .highlighted
+	}
+	
+	override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		//This method will make sure that the use is still touching the area of the button
+		guard let touch = touches.first, enabled else { return }
+		let touchPoint = touch.location(in: self)
+		let touchArea = CGRect(origin: CGPoint(x: -size.width/2, y: -size.height/2), size: size)
+		if !touchArea.contains(touchPoint) {
+			//User is out of the button area, so the button action should not be triggered
+			state = .normal
+		}
+	}
+	
+	override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let _ = touches.first else { return }
+		runAction()
+	}
+	
+	override public func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		state = .normal
+	}
+	
+	private func runAction() {
+		if enabled && state == .highlighted {
+			action?()
+			state = .normal
+		}
+	}
+	
+	//MARK: - Button State
+	
+	///State struct represents a certain state of a SKButtonNode. Each state can have its own SKTexture and font color.
+	private struct State {
+		
+		var texture:SKTexture?
+		var fontColor:SKColor = SKColor.white
+		
+		init(texture:SKTexture?) {
+			self.texture = texture
+		}
+		
+		init(texture:SKTexture?, fontColor:SKColor) {
+			self.texture = texture
+			self.fontColor = fontColor
+		}
+		
+	}
+	
+}
